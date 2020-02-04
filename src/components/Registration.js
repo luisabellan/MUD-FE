90a363d5ebaedb33ccb452cd19e2c0043e630a45
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-02-04 12:18:34 -0600

    added registration code

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,10 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Registration = () => {
+    const [newUser, setNewUser] = useState({first_name: '', last_name: '', username: '', email: '', password: ''});
+
+    const handleChanges = e => {
+        setNewUser({...newUser, [e.target.name]: e.target.value});
+    };
+
+    const register = e => {
+        e.prevent.default();
+
+        axios.post()
+        .then(res => {
+          console.log('Register', res)
+    
+        })
+        .catch(err => {
+          console.log('Registration Error', err.response);
+        })
+  
+        setNewUser('');
+    }
+    
 
     return (
         <div>
+            <form>
+                <div className='registeration-form'>
+                    <label>First Name:</label>
+                    <input
+                        className='registration-form'
+                        type='text'
+                        name='first_name'
+                        placeholder='First Name'
+                        value={newUser.first_name}
+                        onChange= {handleChanges}
+                    />
+                </div>
 
+                <div className='registeration-form'>
+                    <label>Last Name:</label>
+                    <input
+                        className='registeration-form'
+                        type='text'
+                        name='last_name'
+                        placeholder='Last Name'
+                        value={newUser.last_name}
+                        onChange={handleChanges}
+                    />
+                </div>
+
+                <div className='registeration-form'>
+                    <label>Username:</label>
+                    <input
+                        className='registeration-form'
+                        type='text'
+                        name='username'
+                        placeholder='Username'
+                        value={newUser.username}
+                        onChange={handleChanges}
+                    />
+                </div>
+
+                <div className='registeration-form'>
+                    <label>Email:</label>
+                    <input
+                        className='registeration-form'
+                        type='text'
+                        name='email'
+                        placeholder='Email'
+                        value={newUser.email}
+                        onChange={handleChanges}
+                    />
+                </div>
+
+                <div className='registeration-form'>
+                    <label>Password:</label>
+                    <input
+                        className='registeration-form'
+                        type='password'
+                        name='password'
+                        placeholder='create a password'
+                        value={newUser.password}
+                        onChange={handleChanges}
+                    />
+                </div>
+            </form>
         </div>
     )
 }
