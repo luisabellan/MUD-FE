4695353f06a76bc6c8ae141b701cc5266581b21e
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-02-04 11:29:32 -0600

    changed BG color

--- src/components/game-components/GameView.js
+++ src/components/game-components/GameView.js
@@ -44,7 +44,7 @@ export default function GameView() {
                     
                 </StatsGameInfoInput>
                 <GameControls>
-
+                    
                 </GameControls>
             </MainGameContainer>
         </div>
