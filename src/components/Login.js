628157530fbd9a4912cb6c879fce9a4dd784b369
Alexis Panyathong <alexispanyathong@Alexiss-Air.lan>
2020-02-04 16:04:21 -0600

    Updates...
    I styled the Login form.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+//Styled Components
+import { LoginForm, LoginContext, LoginBtn, LoginHeader } from './StyledWidgets';
+
 // Login
 const Login = () => {
     const [user, setNewUser] = useState({username: '', password: ''});
@@ -23,31 +26,37 @@ const Login = () => {
 
     return (
         <div>
-           <form>
-               <div className='login-form'>
-                   <label>Username:</label>
-                   <input
-                        className='login-form'
-                        type='text'
-                        name='username'
-                        placeholder='username'
-                        value={user.username}
-                        onChange={handleChanges}
-                   />
-               </div>
+           <LoginForm>
+               <LoginContext>
+                   <LoginHeader>
+                    <h1>Welcome Back</h1>
+                   </LoginHeader>
+                <div className='login-form'>
+                    <label>Username:</label>
+                    <input
+                            className='login-form'
+                            type='text'
+                            name='username'
+                            placeholder='username'
+                            value={user.username}
+                            onChange={handleChanges}
+                    />
+                </div>
 
-               <div className='login-form'>
-                   <label>Password:</label>
-                   <input
-                        className='login-form'
-                        type='password'
-                        name='password'
-                        placeholder='password'
-                        value={user.password}
-                        onChange={handleChanges}
-                   />
-               </div>
-           </form>
+                <div className='login-form'>
+                    <label>Password:</label>
+                    <input
+                            className='login-form'
+                            type='password'
+                            name='password'
+                            placeholder='password'
+                            value={user.password}
+                            onChange={handleChanges}
+                    />
+                </div>
+                <LoginBtn type='submit' onClick={login}>Login</LoginBtn>
+               </LoginContext>
+           </LoginForm>
         </div>
     )
 }
