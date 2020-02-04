03e4368eed2fadb124dea28b411e86d521cd9623
Alexis Panyathong <alexispanyathong@Alexiss-Air.lan>
2020-02-04 14:37:40 -0600

    Updates
    I created the AppRouter and fixed minor bugs and was able to connect the GameView into the routing.

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 //Components
 import Registration from './Registration';
 import Login from './Login';
+import GameView from './game-components/GameView.js';
 
 const AppRouter = () => {
 
@@ -14,12 +15,14 @@ const AppRouter = () => {
                 <Link to='/'>Home</Link>
                 <Link to='/register'>Register</Link>
                 <Link to='/login'>Login</Link>
+                <Link to='/game-view'>Game</Link>
             </div>
 
             <Switch>
                 <Route exact path='/' />
                 <Route path='/register' component={Registration} />
                 <Route path='/login' component={Login} />
+                <Route path='/game-view' component={GameView} />
             </Switch>
 
         </Router>
